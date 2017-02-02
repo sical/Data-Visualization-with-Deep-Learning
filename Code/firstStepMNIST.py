3ee055211c332ee19d5afe062ba7f443b1a63a13
Theo-Jaunet <theo.jaunet.sio@gmail.com>
2017-02-02 09:59:54 +0100

    Update firstStepMNIST.py
     ) was missing at the end

--- Code/firstStepMNIST.py
+++ Code/firstStepMNIST.py
@@ -61,4 +61,4 @@ if __name__ == '__main__':
   parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
                       help='Directory for storing input data')
   FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
