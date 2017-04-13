d52a6bb82c82b15fcedc4e91b22b3c5db67f0ce2
theo <theo.jaunet.sio@gmail.com>
2017-04-13 16:41:19 +0200

    Web api nanonets URl works

--- Code/Web Api/Request_Use.py
+++ Code/Web Api/Request_Use.py
@@ -1,8 +1,18 @@
-
-
 import requests, json
-data = {'file':open("/home/theo/datas/train/a330.jpg", "rb"), \
-    "appId":("", "14e66690-6efb-46c1-a9c7-559d4b406c28")}
-url = "http://demo.nanonets.ai/ImageCategorization/Label/"
-r = requests.post(url, files=data)
-print (r.content)
+
+
+def askbasicurl(url):
+    print(url+'aaaadsqdsq')
+    data = {'url': ("", url), \
+            "appId": ("", "14e66690-6efb-46c1-a9c7-559d4b406c28")}
+    url = "http://demo.nanonets.ai/ImageCategorization/Label/"
+    r = requests.post(url, files=data)
+    return r.content
+
+
+def askbasiclocal():
+    data = {'file': open("image.jpg", "rb"), \
+            "appId": ("", "14e66690-6efb-46c1-a9c7-559d4b406c28")}
+    url = "http://demo.nanonets.ai/ImageCategorization/Label/"
+    r = requests.post(url, files=data)
+    return r.content
